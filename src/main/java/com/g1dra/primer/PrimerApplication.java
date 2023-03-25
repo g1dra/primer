8cd950f21793e1f5db8855ee507685943bdf49f9
g1dra <darko.vucetic7@gmail.com>
2023-03-25 10:51:41 +0100

    init

---
+++ src/main/java/com/g1dra/primer/PrimerApplication.java
@@ -0,0 +1,13 @@
+package com.g1dra.primer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g1dra/primer/PrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1dra.primer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
